d0725404cdcbc26ce83094c0d3869ad8901cc6ed
Vinicius Oliveira <vinicius.oliveira@local>
2024-08-27 16:21:15 -0300

    fix: tratando alerta do select filter

--- src/app/components/Filter.tsx
+++ src/app/components/Filter.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 enum FilterOption {
   ALL = "all",
   COMPLETED = "true",
@@ -9,9 +11,11 @@ type Props = {
 };
 
 export default function Filter({ getTodoList }: Props) {
+  const [option, setOption] = useState<FilterOption>(FilterOption.ALL);
   const handleChange = (event: any) => {
     const { value } = event.target;
     getTodoList(value === "all" ? undefined : value === "true");
+    setOption(value);
   };
 
   return (
@@ -23,12 +27,10 @@ export default function Filter({ getTodoList }: Props) {
         <select
           id="countries"
           className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-          defaultValue={"all"}
           onChange={handleChange}
+          value={option}
         >
-          <option selected value={FilterOption.ALL}>
-            All
-          </option>
+          <option value={FilterOption.ALL}>All</option>
           <option value={FilterOption.COMPLETED}>Completed</option>
           <option value={FilterOption.NOT_COMPLETED}>Not Completed</option>
         </select>
